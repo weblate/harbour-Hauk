5c8af8ffa25c337289476c446a9c3c481496664b
Nathan <nthn@unseen.is>
2026-08-27 22:48:01 +0200

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (44 of 44 strings)
    
    Translation: Harbour Hauk/Translations
    Translate-URL: https://hosted.weblate.org/projects/harbour-hauk/translations/nl/

--- translations/harbour-hauk-nl.ts
+++ translations/harbour-hauk-nl.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../qml/cover/CoverPage.qml" line="28"/>
         <source>Not sharing</source>
-        <translation type="unfinished"></translation>
+        <translation>Delen uit</translation>
     </message>
 </context>
 <context>
@@ -14,113 +14,113 @@
     <message>
         <location filename="../qml/pages/MainPage.qml" line="15"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Instellingen</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="65"/>
         <source>Latitude</source>
-        <translation type="unfinished"></translation>
+        <translation>Breedtegraad</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="71"/>
         <source>Longitude</source>
-        <translation type="unfinished"></translation>
+        <translation>Lengtegraad</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="77"/>
         <source>Accuracy</source>
-        <translation type="unfinished"></translation>
+        <translation>Precisie</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="20"/>
         <location filename="../qml/pages/MainPage.qml" line="232"/>
         <source>Share link</source>
-        <translation type="unfinished"></translation>
+        <translation>Koppeling delen</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="83"/>
         <source>Short ID</source>
-        <translation type="unfinished"></translation>
+        <translation>Korte ID</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="92"/>
         <source>Stop sharing</source>
-        <translation type="unfinished"></translation>
+        <translation>Delen stoppen</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="92"/>
         <source>Start sharing</source>
-        <translation type="unfinished"></translation>
+        <translation>Delen starten</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="108"/>
         <source>Duration</source>
-        <translation type="unfinished"></translation>
+        <translation>Duur</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="128"/>
         <source>30 minutes</source>
-        <translation type="unfinished"></translation>
+        <translation>30 minuten</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="132"/>
         <source>1 hour</source>
-        <translation type="unfinished"></translation>
+        <translation>1 uur</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="136"/>
         <source>2 hours</source>
-        <translation type="unfinished"></translation>
+        <translation>2 uur</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="140"/>
         <source>4 hours</source>
-        <translation type="unfinished"></translation>
+        <translation>4 uur</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="144"/>
         <source>12 hours</source>
-        <translation type="unfinished"></translation>
+        <translation>12 uur</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="154"/>
         <source>Update interval</source>
-        <translation type="unfinished"></translation>
+        <translation>Updatefrequentie</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="174"/>
         <source>10 seconds</source>
-        <translation type="unfinished"></translation>
+        <translation>10 seconden</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="178"/>
         <source>30 seconds</source>
-        <translation type="unfinished"></translation>
+        <translation>30 seconden</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="182"/>
         <source>1 minute</source>
-        <translation type="unfinished"></translation>
+        <translation>1 minuut</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="186"/>
         <source>2 minutes</source>
-        <translation type="unfinished"></translation>
+        <translation>2 minuten</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="190"/>
         <source>5 minutes</source>
-        <translation type="unfinished"></translation>
+        <translation>5 minuten</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="200"/>
         <source>Server:</source>
-        <translation type="unfinished"></translation>
+        <translation>Server:</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="201"/>
         <source>Not configured</source>
-        <translation type="unfinished"></translation>
+        <translation>Niet geconfigureerd</translation>
     </message>
 </context>
 <context>
@@ -128,108 +128,108 @@
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="17"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Instellingen</translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="21"/>
         <source>Server</source>
-        <translation type="unfinished"></translation>
+        <translation>Server</translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="27"/>
         <source>https://your-server.com/hauk</source>
-        <translation type="unfinished"></translation>
+        <translation>https://je-server.com/hauk</translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="29"/>
         <source>Server URL</source>
-        <translation type="unfinished"></translation>
+        <translation>Server-URL</translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="35"/>
         <source>Authentication</source>
-        <translation type="unfinished"></translation>
+        <translation>Aanmelding</translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="41"/>
         <source>Username (optional)</source>
-        <translation type="unfinished"></translation>
+        <translation>Gebruikersnaam (optioneel)</translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="43"/>
         <source>Username</source>
-        <translation type="unfinished"></translation>
+        <translation>Gebruikersnaam</translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="50"/>
         <location filename="../qml/pages/SettingsPage.qml" line="52"/>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Wachtwoord</translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="58"/>
         <source>Defaults</source>
-        <translation type="unfinished"></translation>
+        <translation>Standaardinstellingen</translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="63"/>
         <source>Default duration</source>
-        <translation type="unfinished"></translation>
+        <translation>Standaardduur</translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="82"/>
         <source>30 minutes</source>
-        <translation type="unfinished"></translation>
+        <translation>30 minuten</translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="86"/>
         <source>1 hour</source>
-        <translation type="unfinished"></translation>
+        <translation>1 uur</translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="90"/>
         <source>2 hours</source>
-        <translation type="unfinished"></translation>
+        <translation>2 uur</translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="94"/>
         <source>4 hours</source>
-        <translation type="unfinished"></translation>
+        <translation>4 uur</translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="98"/>
         <source>12 hours</source>
-        <translation type="unfinished"></translation>
+        <translation>12 uur</translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="106"/>
         <source>Default update interval</source>
-        <translation type="unfinished"></translation>
+        <translation>Standaardupdatefrequentie</translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="125"/>
         <source>10 seconds</source>
-        <translation type="unfinished"></translation>
+        <translation>10 seconden</translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="129"/>
         <source>30 seconds</source>
-        <translation type="unfinished"></translation>
+        <translation>30 seconden</translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="133"/>
         <source>1 minute</source>
-        <translation type="unfinished"></translation>
+        <translation>1 minuut</translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="137"/>
         <source>2 minutes</source>
-        <translation type="unfinished"></translation>
+        <translation>2 minuten</translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="141"/>
         <source>5 minutes</source>
-        <translation type="unfinished"></translation>
+        <translation>5 minuten</translation>
     </message>
 </context>
 </TS>
